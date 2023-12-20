038ee05cc4edcffed49cfbaeb2f0f5a387ce07af
vanessa082 <wahvanessa22@gmail.com>
2023-12-20 17:52:42 +0100

    correcting linter

--- script.js
+++ script.js
@@ -5,47 +5,47 @@ const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
-next.addEventListener(click, () =>{
-    currentActive++
+next.addEventListener(click, () => {
+  currentActive++
 
-    if(currentActive > circles.length){
-       currentActive = circles.length 
-    }
+  if(currentActive > circles.length) {
+    currentActive = circles.length
+  }
 
-    update()
+  update()
 })
 
-prev.addEventListener(click, () =>{
-    currentActive--
+prev.addEventListener(click, () => {
+  currentActive--
 
-    if(currentActive < 1 ){
-       currentActive = 1 
-    }
+  if(currentActive < 1) {
+    currentActive = 1
+  }
 
-    update()
+  update()
 })
 
-function update(){
-    circles.forEach((circle,idx) =>{
-        if(idx< currentActive){
-            circle.classlist.add('active')
-        }else{
-            circle.classList.remove('active')
-        }
-    })
+function update() {
+  circles.forEach((circle, idx) => {
+    if(idx < currentActive) {
+      circle.classlist.add('active')
+    } else {
+      circle.classList.remove('active')
+    }
+  })
 
 
-    const actives = document.querySelectorAll('.active')
+  const actives = document.querySelectorAll('.active')
 
-    progress.style.width = (actives.length -1)/( circles.length - 1) *
+  progress.style.width = (actives.length - 1) / (circles.length - 1) *
     100 + '%'
 
-    if(currentActive === 1){
-        prev.disabled = true
-    } else if(currentActive === circles.length){
-        next.disabled = true
-    } else{
-        prev.disabled = false
-        next.disabled = false
-    }
+  if(currentActive === 1) {
+    prev.disabled = true
+  } else if(currentActive === circles.length) {
+    next.disabled = true
+  } else {
+    prev.disabled = false
+    next.disabled = false
+  }
 }
